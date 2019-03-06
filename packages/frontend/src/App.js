4946c3ab26ad823de87a0cc2099858d7bb0c75c5
dance2die <sungmkim80@gmail.com>
2019-03-06 13:23:33 -0500

    Created a shared hook

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-import SharedComponent from "shared";
+import SharedComponent, { useSharedValue } from "shared";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+function App() {
+  const [count, setCount] = React.useState(0);
+  const shared = useSharedValue();
+
+  return (
+    <div className="App">
+      <header>
         <SharedComponent title={"Shared Component!"} />
+      </header>
+      <div>
+        <label>Shared value => {shared}</label>
       </div>
-    );
-  }
+      {/* <section>
+        <div>
+          <label>Count: {count}</label>
+        </div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+      </section> */}
+    </div>
+  );
 }
 
 export default App;
